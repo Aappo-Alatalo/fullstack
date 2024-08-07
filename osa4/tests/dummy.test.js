b744adc29d717f9547ef1dda4108a5e5836221d0
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-08-07 19:06:43 +0300

    Add 4.6 exercise

--- osa4/tests/dummy.test.js
+++ osa4/tests/dummy.test.js
@@ -9,71 +9,71 @@ test('dummy returns one', () => {
   assert.strictEqual(result, 1)
 })
 
+const emptyList = []
+
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+]
+
 describe('total likes', () => {
-    const emptyList = []
-
-    const listWithOneBlog = [
-      {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-      }
-    ]
-
-    const blogs = [
-        {
-          _id: "5a422a851b54a676234d17f7",
-          title: "React patterns",
-          author: "Michael Chan",
-          url: "https://reactpatterns.com/",
-          likes: 7,
-          __v: 0
-        },
-        {
-          _id: "5a422aa71b54a676234d17f8",
-          title: "Go To Statement Considered Harmful",
-          author: "Edsger W. Dijkstra",
-          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-          likes: 5,
-          __v: 0
-        },
-        {
-          _id: "5a422b3a1b54a676234d17f9",
-          title: "Canonical string reduction",
-          author: "Edsger W. Dijkstra",
-          url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-          likes: 12,
-          __v: 0
-        },
-        {
-          _id: "5a422b891b54a676234d17fa",
-          title: "First class tests",
-          author: "Robert C. Martin",
-          url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-          likes: 10,
-          __v: 0
-        },
-        {
-          _id: "5a422ba71b54a676234d17fb",
-          title: "TDD harms architecture",
-          author: "Robert C. Martin",
-          url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-          likes: 0,
-          __v: 0
-        },
-        {
-          _id: "5a422bc61b54a676234d17fc",
-          title: "Type wars",
-          author: "Robert C. Martin",
-          url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-          likes: 2,
-          __v: 0
-        }  
-    ]
-    
     test('of empty list is zero', () => {
         const result = listHelper.totalLikes(emptyList)
         assert.strictEqual(result, 0)
@@ -88,4 +88,48 @@ describe('total likes', () => {
         const result = listHelper.totalLikes(blogs)
         assert.strictEqual(result, 36)
     })
-  })
+})
+
+describe('favourite blog', () => {
+    test('empty list returns null', () => {
+        const result = listHelper.favoriteBlog(emptyList)
+        assert.strictEqual(result, null)
+    })
+
+    test('list of one blog returns that blog', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        assert.notStrictEqual(result, { // CANNOT BE STRICTLY EQUAL SINCE NOT SAME OBJECT
+            title: listWithOneBlog[0].title,
+            author: listWithOneBlog[0].author,
+            likes: listWithOneBlog[0].likes
+        })
+    })
+
+    test('many blogs', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        assert.strictEqual(result.likes, 12)
+    })
+})
+
+describe('author with most blogs', () => {
+    test('empty list returns null', () => {
+        const result = listHelper.mostBlogs(emptyList)
+        assert.strictEqual(result, null)
+    })
+
+    test('list with one blog returns that author', () => {
+        const result = listHelper.mostBlogs(listWithOneBlog)
+        assert.notStrictEqual(result, {
+            author: listWithOneBlog[0].author,
+            blogs: 1
+        })
+    })
+
+    test('many blogs', () => {
+        const result = listHelper.mostBlogs(blogs)
+        assert.notStrictEqual(result, {
+            author: "Robert C. Martin",
+            blogs: 3
+        })
+    })
+})
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -5,8 +5,46 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
-  
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const mostLikedBlog = blogs.reduce((prev, current) => {
+        return (prev.likes > current.likes) ? prev : current
+    })
+
+    return {
+        title: mostLikedBlog.title,
+        author: mostLikedBlog.author,
+        likes: mostLikedBlog.likes
+    }
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const authorBlogCounts = blogs.reduce((acc, blog) => {
+        acc[blog.author] = (acc[blog.author] || 0) + 1
+        return acc
+    }, {})
+
+    const mostBlogsAuthor = Object.keys(authorBlogCounts).reduce((a, b) => {
+        return authorBlogCounts[a] > authorBlogCounts[b] ? a : b
+    })
+
+    return {
+        author: mostBlogsAuthor,
+        blogs: authorBlogCounts[mostBlogsAuthor]
+    }
+}
+    
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
