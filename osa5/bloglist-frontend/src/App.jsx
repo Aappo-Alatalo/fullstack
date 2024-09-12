90ee0ab554c3d04941f0f8216e817716201b0f77
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-09-12 20:40:52 +0300

    Add exercise 5.2

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -5,7 +5,7 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
 
@@ -15,6 +15,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -22,14 +31,23 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
-    } catch (exception) {
-      setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+    } 
+    // catch (exception) {
+    //   setErrorMessage('wrong credentials')
+    //   setTimeout(() => {
+    //     setErrorMessage(null)
+    //   }, 5000)
+    // }
+    catch (exception) {
+      return false
     }
   }
 
@@ -62,9 +80,16 @@ const App = () => {
     )
   }
 
+  const printstuff = () => console.log("button clicked")
+
+
   return (
     <div>
       <h2>blogs</h2>
+
+      {user.name} logged in
+      <button onClick={() => window.localStorage.clear()}>logout</button>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
