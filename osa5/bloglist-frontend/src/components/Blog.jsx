7591a9507621384e0d1d79cd4d95e20796661fe5
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-09-13 22:14:51 +0300

    Add exercise 5.8

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,13 +1,35 @@
 import { useState } from 'react'
+import blogService from '../services/blogs'
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, blogs, setBlogs}) => {
   const [visible, setVisible] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
 
   const hideWhenVisible = { display: visible ? 'none' : '' }
   const showWhenVisible = { display: visible ? '' : 'none' }
 
   const toggleVisibility = () => {
     setVisible(!visible)
+  }
+
+  const handleLike = async () => {
+    const updatedBlog = {
+      user: blog.user.id,
+      likes: likes + 1,
+      author: blog.author,
+      title: blog.title,
+      url: blog.url,
+    }
+
+    try {
+      const returnedBlog = await blogService.update(blog.id, updatedBlog)
+      setLikes(likes + 1)
+
+      setBlogs(blogs.map(b => b.id === blog.id ? returnedBlog : b))
+    } catch (error) {
+      // console.log('Error updating blog')
+      console.log(error)
+    }
   }
 
   const blogStyle = {
@@ -27,7 +49,7 @@ const Blog = ({ blog }) => {
       <div style={showWhenVisible}>
         <button onClick={toggleVisibility}>hide</button>
         <p>{blog.url}</p>
-        <p>{blog.likes} <button>like</button></p>
+        <p>{likes} <button onClick={handleLike}>like</button></p>
         <p>{blog.user.name}</p>
       </div>
     </div>
