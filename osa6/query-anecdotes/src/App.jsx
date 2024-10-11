a436f77f37130cb19bb8e9502260ae5adff7c92a
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-10-11 22:13:04 +0300

    Add exercises 6.21 and 6.22

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,13 +1,21 @@
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation({ 
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
 
   const result = useQuery({
     queryKey: ['anecdotes'],
-    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+    queryFn: getAnecdotes,
     retry: 1
   })
   console.log(JSON.parse(JSON.stringify(result)))
@@ -15,23 +23,16 @@ const App = () => {
   if ( result.isLoading ) {
     return <div>loading data...</div>
   }
-
   if ( result.isError ) {
     return <div>anecdote service not available due to problems in server</div>
   }
+  // At this point we can assume result is success
+  const anecdotes = result.data
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate(anecdote)
   }
 
-  // const anecdotes = [
-  //   {
-  //     "content": "If it hurts, do it more often",
-  //     "id": "47145",
-  //     "votes": 0
-  //   },
-  // ]
-  const anecdotes = result.data
 
   return (
     <div>
